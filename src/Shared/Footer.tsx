26658f6e76235afc624b9a231977e9d7bd512195
Nikolas Damian <nikolas.vasconcelos@supero.com.br>
2020-03-11 13:31:02 -0300

    tab items with icons

--- src/Shared/Footer.tsx
+++ src/Shared/Footer.tsx
@@ -1,17 +1,32 @@
 import * as React from 'react';
-import {StyleSheet, TouchableHighlight, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {Icon} from 'native-base';
+
+const footerIcons = {
+  HOME: {name: 'home-outline', type: 'MaterialCommunityIcons'},
+  BROWSE: {name: 'youtube', type: 'Feather'},
+  SAVED: {name: 'bookmark', type: 'Feather'},
+  ACCOUNT: {name: 'user', type: 'Feather'},
+};
 
 const Footer: React.FC<Props> = ({state, descriptors, navigation}) => {
   console.log(state, descriptors, navigation);
   return (
     <View style={styles.footer}>
-      {state.routes.map(route => (
-        <TouchableHighlight
-          key={route.key}
-          onPress={() => navigation.navigate(route.name)}>
-          <Text>{route.name}</Text>
-        </TouchableHighlight>
-      ))}
+      {state.routes.map(route => {
+        const iconName = footerIcons[`${route.name.toUpperCase()}`].name;
+        const iconType = footerIcons[route.name.toUpperCase()].type;
+        return (
+          <TouchableOpacity
+            key={route.key}
+            onPress={() => navigation.navigate(route.name)}>
+            <View style={styles.footerIconWrapper}>
+              <Icon style={styles.footerIcon} name={iconName} type={iconType} />
+              <Text style={styles.footerText}>{route.name}</Text>
+            </View>
+          </TouchableOpacity>
+        );
+      })}
     </View>
   );
 };
@@ -37,16 +52,19 @@ interface NavRoute {
 
 const styles = StyleSheet.create({
   footer: {
-    height: 65,
+    height: 70,
     position: 'absolute',
     bottom: 0,
     left: 0,
     right: 0,
-    paddingBottom: 10,
+    paddingBottom: 15,
     zIndex: 5,
     backgroundColor: 'white',
     justifyContent: 'space-evenly',
     alignItems: 'center',
     flexDirection: 'row',
   },
+  footerText: {fontSize: 11},
+  footerIcon: {fontSize: 20},
+  footerIconWrapper: {alignItems: 'center'},
 });
